889af7acbff07631fe9f38d46ef5bd776e1727c7
Doha-25 <dohazakibaniyaseen@gmail.com>
2025-09-21 10:19:30 +0300

    Add files via upload
    changed

--- 22.py
+++ 22.py
@@ -7,10 +7,16 @@ root.title("Pack Example")
 button1 = tk.Button(root, text="Button 1")
 button2 = tk.Button(root, text="Button 2")
 button3 = tk.Button(root, text="Button 3")
+button4 = tk.Button(root, text="Button 4")
+button5 = tk.Button(root, text="Button 5")
+button6 = tk.Button(root, text="Button 6")
 
 # Pack the buttons vertically
 button1.pack()
 button2.pack()
 button3.pack()
+button4.pack()
+button5.pack()
+button6.pack()
 
 root.mainloop()
